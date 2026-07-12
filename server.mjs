d00b1ff443d9631627d88e6cd67eaf025053ebfe
B2B Driver Dev <dev@b2b-driver.local>
2026-07-13 01:12:16 +0200

    Change default port from 3000 to 4000 to avoid conflict with other projects

--- server.mjs
+++ server.mjs
@@ -5,7 +5,7 @@ import { initSocketServer } from "./src/server/socket.js";
 
 const dev = process.env.NODE_ENV !== "production";
 const hostname = process.env.HOST || "localhost";
-const port = parseInt(process.env.PORT || "3000", 10);
+const port = parseInt(process.env.PORT || "4000", 10);
 
 const app = next({ dev, hostname, port });
 const handle = app.getRequestHandler();
